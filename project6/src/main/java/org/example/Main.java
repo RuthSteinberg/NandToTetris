f996eb7ffe37215920fb5d72e42fa08ec7eee442
RuthSteinberg <ruth.steinberg@post.runi.ac.il>
2024-12-05 16:02:37 +0200

    push main

--- project6/src/main/java/org/example/Main.java
+++ project6/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file = new File(C:\Users\ruths\OneDrive\02 second year\03 - Nand\NandToTetris\6\add");  // יש להחליף בנתיב לקובץ שלך
+        Parser parser = new Parser(file);
+
+        try {
+            while (parser.hasMoreLines()) {
+                parser.advance();
+                System.out.println("Current instruction: " + parser.instruction);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- project6/src/main/java/org/example/Parser.java
+++ project6/src/main/java/org/example/Parser.java
@@ -1,4 +1,52 @@
 package org.example;
+import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import java.io.BufferedReader;
+
 
 public class Parser {
+    public File inputFile;
+    public BufferedReader reader;
+    public String currentLine;
+    public String instruction;
+
+
+    public Parser(File file) {
+        try {
+            if (file.exists()) {
+                this.inputFile = file;
+                this.reader = new BufferedReader(new FileReader(file));
+                this.currentLine = reader.readLine();
+                this.instruction = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("can't reading from the file");
+        }
+    }
+
+    public boolean hasMoreLines() throws IOException {
+        return currentLine != null; // if currentLine!=null return true - there is more line
+    }
+    public String lineCleaner(String line) {
+        if (line.contains("//")) // if the string contain // shorter the string
+        {
+            line = line.substring(0,line.indexOf("/"));
+        }
+    return line;
+    }
+    public void advance() throws IOException {
+        while (currentLine != null) {
+            if(currentLine.contains("//")) {
+                currentLine = lineCleaner(currentLine);
+            }
+            if (!currentLine.isEmpty())
+            {
+                instruction = currentLine;
+            }
+            currentLine = reader.readLine();
+        }
+    }
 }
+
